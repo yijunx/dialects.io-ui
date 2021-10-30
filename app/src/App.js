3f618d83dd9001d4fc60989a1e6c6e61ee05457f
yijunxu <yijunx@umich.edu>
2021-10-30 09:24:00 +0000

    kind of finished some tailwind tutorial

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Product from "./components/Product";
+import products from "./data/products.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-200 py-4">
+      <div className="md:w-1/2 mx-auto">
+        {products.map((product) => (
+          <Product product={product} key={product.id} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ app/src/components/Product.js
@@ -0,0 +1,21 @@
+function Product({ product: { title, price, description, category, image } }) {
+  return (
+    <div className="flex bg-white w-full mb-5 shadow-sm rounded-lg hover:shadow-2xl transition-shadow duration-300">
+      <div className="w-5/12 p-2">
+        <div
+          style={{ backgroundImage: `url(${image})` }}
+          className="bg-contain bg-no-repeat bg-center w-full h-full"
+        ></div>
+      </div>
+
+      <div className="w-7/12 p-5">
+        <h1 className="md:text-2xl">{title.substr(0, 50)}</h1>
+        <h3 className="text-gray-400">{category}</h3>
+        <p className="text-red-400 mt-4 text-xl md:text-4xl">{price}$</p>
+        <div className="mt-4">{description.substr(0, 100)}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
