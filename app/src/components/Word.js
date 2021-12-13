26ed87ffb4da841295fab473fd3445953a63c658
yijunxu <yijunx@umich.edu>
2021-12-13 15:55:49 +0000

    adjust font size

--- app/src/components/Word.js
+++ app/src/components/Word.js
@@ -1,7 +1,7 @@
 function Word({ word: { title, explanation, pronunciation, usage } }) {
   return (
-    <div className="bg-white w-full mb-5 shadow-sm rounded-lg">
-      <h1 className="text-2xl m-1">{title.substr(0, 50)}</h1>
+    <div className="bg-white w-full mb-2 shadow-sm rounded-lg">
+      <h1 className="text-xl m-1">{title.substr(0, 50)}</h1>
       <h3 className="text-gray-400 m-1">发音：{pronunciation}</h3>
       <div className="m-1">意思：{explanation.substr(0, 100)}</div>
       <div className="m-1">用法：{usage.substr(0, 100)}</div>
