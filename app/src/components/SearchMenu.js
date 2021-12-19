4edc01c9766216e21698d69e29344eb91a842e40
yijunxu <yijunx@umich.edu>
2021-12-19 07:42:42 +0000

    display search in the word page

--- app/src/components/SearchMenu.js
+++ app/src/components/SearchMenu.js
@@ -13,13 +13,32 @@ function SearchMenu({ currentWordQuery }) {
   let tagPart = "";
   let dialectPart = "";
 
-  if (currentWordQuery.title) {
-    titlePart = <div></div>;
+  if (wordQuery.title) {
+    titlePart = (
+      <div className="m-1 text-gray-500 rounded-lg bg-green-100 max-w-max text-center px-2">
+        title: {wordQuery.title}
+      </div>
+    );
+  }
+  console.log(wordQuery);
+  if (wordQuery.tag) {
+    tagPart = (
+      <div className="m-1 text-gray-500 rounded-lg bg-green-100 max-w-max text-center px-2">
+        tag: {wordQuery.tag}
+      </div>
+    );
+  }
+  if (wordQuery.dialect) {
+    dialectPart = (
+      <div className="m-1 text-gray-500 rounded-lg bg-green-100 max-w-max text-center px-2">
+        dialect: {wordQuery.dialect}
+      </div>
+    );
   }
 
   return (
-    <div>
-      <p>search function under construction</p>
+    <div className="flex">
+      {titlePart} {tagPart} {dialectPart}
     </div>
   );
 }
